04643dc96c3706b243ab5941a371518ac2133a29
MarskyMessier <marskymessier@gmail.com>
2024-06-27 18:31:47 +0200

    Added ext tracker

---
+++ config/lorekeeper/ext-tracker/Prompt_Form.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'key' => 'prompt_form',
+    'wiki_key' => 'Prompt_Form',
+        'creators' => json_encode([
+        'MarskyMessier' => 'https://github.com/MarskyMessier',
+        'notecardPasta' => 'https://github.com/notecardPasta'
+    ]),
+    'version' => '1.0.0',
+];
